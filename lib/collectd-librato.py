ffb3b5d0efb5f3911875f3d265e30129d8ad80f8
Mike Heffner <mikeh@fesnel.com>
2012-07-03 09:59:59 -0400

    Rename Regex option to IncludeRegex.

--- lib/collectd-librato.py
+++ lib/collectd-librato.py
@@ -150,8 +150,8 @@ def librato_config(c):
             config['single_value_names'] = True
         elif child.key == 'Source':
             config['source'] = val
-        elif child.key == 'Regex':
-            config['regex'] = val.split(',') if val else []
+        elif child.key == 'IncludeRegex':
+            config['include_regex'] = val.split(',') if val else []
         elif child.key == 'FlushIntervalSecs':
             try:
                 config['flush_interval_secs'] = int(str_to_num(val))
@@ -286,7 +286,7 @@ def librato_write(v, data=None):
         if config['lower_case']:
             metric_name = metric_name.lower()
         
-        regexs = config.get('regex', [])
+        regexs = config.get('include_regex', [])
         matches = len(regexs) == 0
         for regex in regexs:
           if re.match(regex, metric_name):
